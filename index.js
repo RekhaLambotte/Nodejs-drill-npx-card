81dd049b3c814c0bfde422b4084bec1e8acff064
RekhaDev <rekhalambotte246@gmail.com>
2021-04-21 12:24:13 +0200

    creation

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const data = {
+    name: chalk.whiteBright("              Rekha Lambotte"),
+    handle: chalk.white ("Rekha"),
+    work: chalk.white ("Web develeoper junior"),
+    npm: (""),
+    github: (""),
+    Linkedin: (""),
+    npx: (""),
+    npm: (""),
+
+};
+
+const NEWLINE = "\n";
+const EMPTYLINE = "";
+
+console.log(
+    chalk.green(
+        boxen(
+            [
+                `${data.name} / ${data.handle}`,
+                EMPTYLINE,
+                `${data.work}`
+            ].join(NEWLINE),
+            {
+                padding: 1,
+                margin: 1,
+                backgroundColor: "#F8D5FC" ,
+                borderStyle: "doubleSingle",
+                align: "center",
+            },
+        ),
+    ),
+);
